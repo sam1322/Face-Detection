87925e44eb48ba9f1ed6e54b199146202bb7d691
sam1322 <sriram.pant5@gmail.com>
2020-05-20 13:43:32 +0530

    Finished Rendering particles react js

--- src/App.js
+++ src/App.js
@@ -7,88 +7,27 @@ import Particles from 'react-particles-js';
 import './App.css'; 
 import 'tachyons' ;
 
-// const particleOptions = {
-//   particles: {
-//     number : {
-//       value:30 ,
-//       density: {
-//         enable: true,
-//         value_area: 800 
-//       }
-//     } 
-//   }
-// }
+const particleOptions = {
+  particles: {
+    number : {
+      value:100 ,
+      density: {
+        enable: true,
+        value_area: 800 
+      }
+    } 
+  }
+}
 
 class App extends React.Component{
   render(){
   return (
     <div className="App"> 
-    {/* <Particles className ='particles' 
-      param ={particleOptions}
-    /> */}
+    <Particles className ='particles' 
+      params ={particleOptions}
+    />
 
-    <Particles
-    style={{
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      // zIndex: -1
-    }}
-    params={{
-      number: {
-        value: 900,
-        density: {
-          enable: true,
-          value_area: 800
-        }
-      },
-      line_linked: {
-        enable: true,
-        distance: 150,
-        color: "#ffffff",
-        opacity: 0.4,
-        width: 1
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        polygon: {
-          nb_sides: 7
-        }
-      },
-      "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-          "onhover": {
-            "enable": true,
-            "mode": "grab"
-          },
-          "onclick": {
-            "enable": true,
-            "mode": "push"
-          },
-          "resize": true
-        },
-        "modes": {
-          "grab": {
-            "distance": 400,
-            "line_linked": {
-              "opacity": 1
-            }
-          },
-          "repulse": {
-            "distance": 200,
-            "duration": 0.4
-          }
-        }
-      },
-      "retina_detect": true
-    }} />
+ 
      <Navigation/>
       <Logo/>
       <Rank/>
